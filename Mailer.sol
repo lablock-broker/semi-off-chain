c4861a558687a5b398c4932e3a2fb6931e966089
Saman H. Pasha <51169592+saman-pasha@users.noreply.github.com>
2021-08-15 17:02:05 +0430

    Update Mailer.sol
    Events added

--- Mailer.sol
+++ Mailer.sol
@@ -14,6 +14,9 @@ contract Mailer {
     address private _owner;
     mapping (address => string) private groups;
     
+    event PersonAdded(address indexed person, string group);
+    event PersonRemoved(address indexed person);
+    
     modifier isOwner() {
         require(msg.sender == _owner, "not authorized");
         _;
@@ -26,11 +29,13 @@ contract Mailer {
     
     function addToGroup(address person, string calldata group) isOwner external returns (bool) {
         groups[person] = group;
+        emit PersonAdded(person, group);
         return true;
     }
     
     function removeFromGroup(address person) isOwner external returns (bool) {
         delete groups[person];
+        emit PersonRemoved(person);
         return true;
     }
     
@@ -39,11 +44,12 @@ contract Mailer {
     }
     
     function validateMail(Mail calldata mail) external view returns (bool isValidated, string memory reason) {
+        bytes32 sender_group = keccak256(bytes(groups[msg.sender]));
         bytes32 from_group = keccak256(bytes(groups[mail.from]));
         bytes32 to_group = keccak256(bytes(groups[mail.to]));
         
-        require(from_group == ADMIN_GROUP || msg.sender == mail.from, "from should be sender or admin");
-        require(from_group == ADMIN_GROUP || from_group == to_group, "cross group mail not allowed");
+        require(sender_group == ADMIN_GROUP || msg.sender == mail.from, "from should be sender or admin");
+        require(sender_group == ADMIN_GROUP || from_group == to_group, "cross group mail not allowed");
         
         if (bytes(mail.contents).length == 0) {
             isValidated = false;
