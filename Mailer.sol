4bec236469596b9b1e56dd13e42f72080a5a4a88
Saman H. Pasha <51169592+saman-pasha@users.noreply.github.com>
2021-08-20 00:04:08 +0430

    Update Mailer.sol

--- Mailer.sol
+++ Mailer.sol
@@ -4,20 +4,25 @@ pragma solidity =0.8.6;
 
 
 contract Mailer {
-    struct Mail {
-        address from;
-        address to;
-        string  contents;
+    struct Person {
+        string name;
+        address wallet;
     }
     
-    bytes32 constant ADMIN_GROUP = keccak256("admin");
+    struct Mail {
+        Person from;
+        Person to;
+        string contents;
+    }
+    
+    bytes32 private constant ADMIN_GROUP = keccak256("admin");
     address private _owner;
     mapping (address => string) private groups;
     
     event PersonAdded(address indexed person, string group);
     event PersonRemoved(address indexed person);
     
-    modifier isOwner() {
+    modifier onlyOwner() {
         require(msg.sender == _owner, "not authorized");
         _;
     }
@@ -27,13 +32,13 @@ contract Mailer {
         groups[_owner] = "admin";
     }
     
-    function addToGroup(address person, string calldata group) isOwner external returns (bool) {
+    function addToGroup(address person, string calldata group) onlyOwner external returns (bool) {
         groups[person] = group;
         emit PersonAdded(person, group);
         return true;
     }
     
-    function removeFromGroup(address person) isOwner external returns (bool) {
+    function removeFromGroup(address person) onlyOwner external returns (bool) {
         delete groups[person];
         emit PersonRemoved(person);
         return true;
@@ -45,10 +50,10 @@ contract Mailer {
     
     function validateMail(Mail calldata mail) external view returns (bool isValidated, string memory reason) {
         bytes32 sender_group = keccak256(bytes(groups[msg.sender]));
-        bytes32 from_group = keccak256(bytes(groups[mail.from]));
-        bytes32 to_group = keccak256(bytes(groups[mail.to]));
+        bytes32 from_group = keccak256(bytes(groups[mail.from.wallet]));
+        bytes32 to_group = keccak256(bytes(groups[mail.to.wallet]));
         
-        require(sender_group == ADMIN_GROUP || msg.sender == mail.from, "from should be sender or admin");
+        require(sender_group == ADMIN_GROUP || msg.sender == mail.from.wallet, "from should be sender or admin");
         require(sender_group == ADMIN_GROUP || from_group == to_group, "cross group mail not allowed");
         
         if (bytes(mail.contents).length == 0) {
